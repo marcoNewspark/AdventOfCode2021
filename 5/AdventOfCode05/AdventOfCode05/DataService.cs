d08ae13b54de89842e69e65d7ac53a567f5b978a
Marco Littel <ml@newspark.nl>
2021-12-06 21:22:54 +0100

    Day 5 in progress

---
+++ 5/AdventOfCode05/AdventOfCode05/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode05
+{
+    public class DataService
+    {
+        private const string _fileIn = "..\\..\\..\\input.txt";
+        public List<int> LanternFish { get; set; }
+
+        public DataService()
+        {
+            LanternFish = new List<int>();
+            
+
+            List<string> input = new List<string>();
+            input.AddRange(File.ReadAllText(_fileIn).Split(","));
+            input.ForEach(x =>
+                LanternFish.Add(int.Parse(x))
+                );
+
+            return;
+        }
+
+    }
+}
---
+++ 5/AdventOfCode05/AdventOfCode05/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdventOfCode05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new DataService();
+
+            var l = new LanternfishService(d.LanternFish);
+
+            var result = l.CalculatePopulationWithArrays(256);
+            //var result = l.CalculatePopulation (256);
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
